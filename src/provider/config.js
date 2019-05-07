f36ef21811dfba9f503a3708441f64bd8dff51f3
linhtdb <linhtdb@agiletech.vn>
2019-05-08 01:53:26 +0700

    up

---
+++ src/provider/config.js
@@ -0,0 +1,2 @@
+// eslint-disable-next-line import/prefer-default-export
+export const apiBase = 'http://192.168.100.72/cakephp';
---
+++ src/provider/index.js
@@ -0,0 +1,138 @@
+/* eslint-disable vars-on-top */
+/* eslint-disable no-var */
+/* eslint-disable no-loop-func */
+/* eslint-disable guard-for-in */
+/* eslint-disable no-param-reassign */
+import axios from 'axios';
+import { apiBase } from './config';
+// import localStorage from 'localStorage';
+
+const headers = {
+  // 'Access-Control-Allow-Origin': '*',
+};
+
+// headers.append("GET", "POST", "OPTIONS");
+
+function preSendData(data) {
+  // eslint-disable-next-line no-restricted-syntax
+  for (const prop in data) {
+    const dataElement = data[prop];
+    if (
+      dataElement == null ||
+      dataElement === undefined ||
+      dataElement === 'null' ||
+      dataElement === '' ||
+      dataElement === 'undefined'
+    ) {
+      delete data[prop];
+    }
+  }
+
+  return data;
+}
+const request = axios.create({
+  baseURL: apiBase,
+  headers,
+});
+// axios.defaults.baseURL = apiBase;
+// axios.defaults.headers.common = { "X-Requested-With": "XMLHttpRequest" };
+// axios.defaults.baseURL = process.env.NODE_ENV !== "production" ? apiBase : "";
+// const methods = ["get", "put", "post", "delete", "patch"];
+export const get = (endpoint, config = {}, getlocalStorage = true) => {
+  if (getlocalStorage && localStorage.getItem('token')) {
+    config = {
+      ...config,
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+    };
+  }
+
+  return request.get(endpoint, config);
+};
+export const post = (
+  endpoint,
+  data,
+  config = {
+    headers: null,
+  },
+) => {
+  // eslint-disable-next-line quotes
+  if (localStorage.getItem("token") && !config.headers) {
+    config = {
+      ...config,
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+    };
+  }
+  data = preSendData(data);
+
+  const formData = new FormData();
+  if (data && data.hasFile) {
+    config = {
+      ...config,
+      header: {
+        ...config.headers,
+        'Content-Type': 'multipart/form-data',
+      },
+    };
+    delete data.hasFile;
+    // eslint-disable-next-line no-restricted-syntax
+    // eslint-disable-next-line vars-on-top
+    // eslint-disable-next-line no-restricted-syntax
+    // eslint-disable-next-line vars-on-top
+    // eslint-disable-next-line no-restricted-syntax
+    for (var prop in data) {
+      console.log('prop: ', data[prop]);
+      if (Array.isArray(data[prop])) {
+        data[prop].forEach((dt) => {
+          formData.append(prop, dt);
+        });
+      } else {
+        formData.append(prop, data[prop]);
+      }
+
+      delete data[prop];
+    }
+    data = formData;
+  }
+
+  return request.post(endpoint, data, config);
+};
+
+export const puts = (
+  endpoint,
+  data,
+  config = {
+    headers: {},
+  },
+) => {
+  if (localStorage.getItem('token')) {
+    config = {
+      ...config,
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+    };
+  }
+  return request.put(endpoint, data, config);
+};
+
+export const deletes = (
+  endpoint,
+  data,
+  config = {
+    headers: {},
+  },
+) => {
+  if (localStorage.getItem('token')) {
+    config = {
+      ...config,
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+    };
+  }
+  return request.delete(endpoint, config);
+};
